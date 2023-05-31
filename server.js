84602352e105b400ae5b189cfd0bfb9340b1f0ca
Aman <liplap2020@gmail.com>
2023-05-31 14:05:51 -0300

    m10-5

--- server.js
+++ server.js
@@ -99,6 +99,6 @@ const usersCollection = db.collection('Payment');
 //     console.error('Error adding document:', error);
 //   });
       // file1.sayHello(amount,fname,lname,email,reference);
-    res.send(responseBody);
+    res.send(responseBody.data.first_name);
   });
 });
