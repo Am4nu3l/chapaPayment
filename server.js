34dcbd72240f0963bd0309a6e4b4018a8d73949a
Aman <liplap2020@gmail.com>
2023-05-31 12:42:29 -0300

    please

--- server.js
+++ server.js
@@ -71,26 +71,6 @@ app.get('/pay', function(req, res) {
   request(options, function (error, response) {
     if (error) throw new Error(error);
     res.send(responseBody);
-    const admin=require('firebase-admin')
-    const serviceAccount =require('./serviceAccountKey.json')
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-const db = admin.firestore();
-    const collectionRef = db.collection('Ambulance');
-
-  // Fetch documents from the collection
-  collectionRef.get()
-    .then((querySnapshot) => {
-      const data = [];
-      querySnapshot.forEach((doc) => {
-        
-       console.log(doc.data());
-      });
-    })
-    .catch((error) => {
-      console.error('Error fetching collection data:', error);
-      res.status(500).json({ error: 'Failed to fetch collection data' });
-    });
+    
   });
 });
