677eb67f153c63ff746548deda8f675002c80496
Aman <liplap2020@gmail.com>
2023-06-03 14:19:28 -0300

    with update membership

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 const db = admin.firestore();
+const updateMembership=db.collection('User');
 const usersCollection = db.collection('Payment');
 // // Create a new document with an automatically generated ID
 usersCollection.add({
@@ -20,6 +21,20 @@ usersCollection.add({
   .catch((error) => {
     console.error('Error adding document:', error);
   });
+  updateMembership.get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    if (doc.data().Email === email) {
+    
+      updateMembership.doc(doc.id).update({ membership: 'premium' })
+        .then(() => {
+          console.log('Membership successfully updated');
+        })
+        .catch((error) => {
+          console.error('Error updating data: ', error);
+        });
+    }
+  });
+});
 }
 
 module.exports = { sayHello };
